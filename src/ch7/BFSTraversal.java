85dbc5000c3c35657f88571622fae15c7c629c34
Heshan Hussen <hesham.200053@gmail.com>
2022-06-06 22:51:24 +0200

    isSameTree solutions

--- src/ch7/BFSTraversal.java
+++ src/ch7/BFSTraversal.java
@@ -21,6 +21,8 @@ public class BFSTraversal {
         adj[x].add(y);      /* adding an edge to the adjacency list (edges are bidirectional in this example) */
     }
 
+
+
     void BFS(int startNode) {
         var visitedNodes = new HashSet<Integer>();
         que.add(startNode);      /* root node is added to the top of the queue */
@@ -41,4 +43,103 @@ public class BFSTraversal {
         }
     }
 
+
+//         if(p==null && q==null)
+//            return true;
+//        if(p==null || q==null)
+//            return false;
+//        if(p.val!=q.val)
+//            return false;
+//        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
+    public void treeBFS(TreeNode root) {
+        var queue = new ArrayDeque<TreeNode>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            System.out.printf(root.val + " ");
+
+            if (root.left != null ) {
+                queue.add(root.left);
+            }
+
+            if (root.right != null ) {
+                queue.add(root.right);
+            }
+        }
+    }
+
+    // really invasive isSameTree
+    // Take home lesson: compare node => structure => value
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if(p == null && q == null){
+            return true;
+        }
+
+        //structure
+        if(p == null || q == null){
+            return false;
+        }
+
+        //val
+        if(p.val != q.val){
+            return false;
+        }
+
+        // [1,2]
+        // [1,null,2]
+        Queue<TreeNode> pQueue = new ArrayDeque<TreeNode>();
+        Queue<TreeNode> qQueue = new ArrayDeque<TreeNode>();
+
+        pQueue.add(p);
+        qQueue.add(q);
+
+        while (pQueue.size() != 0 && qQueue.size() != 0) {
+            p = pQueue.poll(); // 1
+            q = qQueue.poll(); // 1
+
+
+            if(p.val != q.val)  return false;
+
+            // p.left = 2
+            // q.left = null
+            if(p.left != null && q.left == null ) return false;
+            if(p.left == null && q.left != null ) return false;
+
+            if(p.right == null && q.right != null ) return false;
+            if(p.right != null && q.right == null ) return false;
+
+            if(p.left != null && q.left != null ) {
+                pQueue.add(p.left); // noExec
+                qQueue.add(q.left); // noExec
+            }
+
+            if(p.right != null && q.right != null ) {
+                pQueue.add(p.right); // noExec
+                qQueue.add(q.right); // noExec
+            }
+
+
+        }
+        return true;
+
+    }
+
+    // optimum solution
+    public boolean isSameTreeSmart(TreeNode p, TreeNode q) {
+
+        if(p == null && q == null) return true;
+
+        //structure
+        if(p == null || q == null) return false;
+
+        //val
+        if(p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+
+    }
 }
--- src/ch7/ChapterSeven.java
+++ src/ch7/ChapterSeven.java
@@ -1,5 +1,7 @@
 package ch7;
 
+import com.sun.source.tree.Tree;
+
 public class ChapterSeven {
 
 
@@ -25,15 +27,26 @@ public class ChapterSeven {
 //        graph.insertEdge(5, 3);
 
         // bidirectional and with self-loop
-        graph.insertEdge(0, 1);
-        graph.insertEdge(0, 2);
-        graph.insertEdge(1, 2);
-        graph.insertEdge(2, 0);
-        graph.insertEdge(2, 3);
-        graph.insertEdge(3, 3);
+//        graph.insertEdge(0, 1);
+//        graph.insertEdge(0, 2);
+//        graph.insertEdge(1, 2);
+//        graph.insertEdge(2, 0);
+//        graph.insertEdge(2, 3);
+//        graph.insertEdge(3, 3);
+//        System.out.println("Breadth First Traversal for the graph is:");
+//        graph.BFS(2);
 
-        System.out.println("Breadth First Traversal for the graph is:");
-        graph.BFS(2);
+
+        var N_1 = new TreeNode(1);
+        var N_9 = new TreeNode(9);
+        var N_8 = new TreeNode(8);
+        var N_7 = new TreeNode(7);
+
+        var N_3 = new TreeNode(3, N_1, N_9);
+        var N_2 = new TreeNode(2, N_8, N_7);
+        var N_5 = new TreeNode(5, N_3,N_2);
+
+        graph.treeBFS(N_5);
 
 // =======================================================================================================================
 
